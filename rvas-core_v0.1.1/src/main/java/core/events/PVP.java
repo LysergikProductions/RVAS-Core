2ef82440cf0ec97363dd471cf70cf0f3f877e6d0
LysergikProductions <61183022+LysergikProductions@users.noreply.github.com>
2021-04-07 04:23:50 -0400

    updated pom and added missing code in pvp

--- rvas-core_v0.1.1/src/main/java/core/events/PVP.java
+++ rvas-core_v0.1.1/src/main/java/core/events/PVP.java
@@ -15,25 +15,27 @@ import org.bukkit.event.entity.EntityResurrectEvent;
 import core.backend.Config;
 import core.backend.PlayerMeta;
 
-@EventHandler
-public void onKill(PlayerDeathEvent e) {
-	String killed = e.getEntity().getName();
-	String killer = e.getEntity().getKiller().getName();
-	String killerLoc = e.getEntity().getKiller().getLocation().getX()+", "+e.getEntity().getKiller().getLocation().getY()+", "+e.getEntity().getKiller().getLocation().getZ()
-
-	//e.setDeathMessage(ChatColor.RED + killed + " has been slain by " + killer);
+public class PVP implements Listener {
+	@EventHandler
+	public void onKill(PlayerDeathEvent e) {
+		String killed = e.getEntity().getName();
+		String killer = e.getEntity().getKiller().getName();
+		String killerLoc = e.getEntity().getKiller().getLocation().getX()+", "+e.getEntity().getKiller().getLocation().getY()+", "+e.getEntity().getKiller().getLocation().getZ();
 	
-	if (Config.getValue("debug").equals("true") && Config.getValue("devesp").equals("false")) {
-		if (killed != null) {
-			System.out.println("[core.events.pvp] "+killer+" killed "+killed+" from "+killerLoc);
-			System.out.println("[core.events.pvp] Incrementing killTotal for "+killer);
+		//e.setDeathMessage(ChatColor.RED + killed + " has been slain by " + killer);
+		
+		if (Config.getValue("debug").equals("true") && Config.getValue("devesp").equals("false")) {
+			if (killed != null) {
+				System.out.println("[core.events.pvp] "+killer+" killed "+killed+" from "+killerLoc);
+				System.out.println("[core.events.pvp] Incrementing killTotal for "+killer);
+			}
+		} else if (Config.getValue("devesp").equals("false")) {
+			if (killed != null) {
+				System.out.println("[core] "+killer+" killed "+killed+" from "+killerLoc);
+			}
 		}
-	} else if (Config.getValue("devesp").equals("false")) {
-		if (killed != null) {
-			System.out.println("[core] "+killer+" killed "+killed+" from "+killerLoc);
+		if (killed && killer != null) {
+			PlayerMeta.incKillTotal(e.getEntity().getKiller());
 		}
-	}
-	if (killed && killer != null) {
-		PlayerMeta.incKillTotal(e.getEntity().getKiller())
 	}
 }
