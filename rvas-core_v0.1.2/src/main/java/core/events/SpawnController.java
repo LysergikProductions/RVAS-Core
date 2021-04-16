f7f32f2f1909b71f4a6548f2813df449d21c4867
LysergikProductions <61183022+LysergikProductions@users.noreply.github.com>
2021-04-16 02:41:41 -0400

    further cleaned up SpawnController

--- rvas-core_v0.1.2/src/main/java/core/events/SpawnController.java
+++ rvas-core_v0.1.2/src/main/java/core/events/SpawnController.java
@@ -5,6 +5,8 @@ import java.util.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 import org.bukkit.entity.Player;
@@ -18,10 +20,6 @@ import core.backend.Config;
 
 public class SpawnController implements Listener {
 	
-	public static ArrayList<Material> BannedSpawnFloors = new ArrayList<>(); {
-		BannedSpawnFloors.addAll(Arrays.asList(Material.AIR));
-	}
-	
 	double max_x; double max_z;
 	double min_x; double min_z;
 	
@@ -30,17 +28,72 @@ public class SpawnController implements Listener {
 	Double config_min_x = Double.parseDouble(Config.getValue("spawn.min.X"));
 	Double config_min_z = Double.parseDouble(Config.getValue("spawn.min.Z"));
 	
+	public static ArrayList<Material> BannedSpawnFloors = new ArrayList<>(); {
+		BannedSpawnFloors.addAll(Arrays.asList(Material.AIR));
+	}
+	
 	public int getRandomNumber(int min, int max) {
 	    return (int) ((Math.random() * (max - min)) + min);
+	}
+	
+	public Location getRandomSpawn(World thisWorld, Location newSpawnLocation) {
+		boolean valid_spawn_location = false;
+		
+		// if a configured value can't be parsed to Double, set to default
+		if (config_max_x.isNaN()) max_x = 420.0; else max_x = config_max_x.doubleValue();
+		if (config_max_z.isNaN()) max_z = 420.0; else max_z = config_max_z.doubleValue();	
+		if (config_min_x.isNaN()) min_x = -420.0; else min_x = config_min_x.doubleValue();
+		if (config_min_z.isNaN()) min_z = -420.0; else min_z = config_min_z.doubleValue();
+		
+		// get random x, z coords and check them top-down from y256 for validity
+		while (!valid_spawn_location) {
+			
+			double tryLocation_x = getRandomNumber((int)min_x, (int)max_x);
+			double tryLocation_z = getRandomNumber((int)min_z, (int)max_z);
+			
+			System.out.println("RVAS: Checking coords for respawn: " + tryLocation_x + ", " + tryLocation_z);
+			
+			int y = 257;
+			while (y > 1) {
+				
+				Location headLoc = new Location(thisWorld, tryLocation_x, (double)y, tryLocation_z);
+				Location legsLoc = new Location(thisWorld, tryLocation_x, (double)y-1, tryLocation_z);
+				Location floorLoc = new Location(thisWorld, tryLocation_x, (double)y-2, tryLocation_z);
+				
+				Block headBlock = headLoc.getBlock();
+				Block legsBlock = legsLoc.getBlock();
+				Block floorBlock = floorLoc.getBlock();
+				
+				y--;
+				
+				if (!headBlock.getType().equals(Material.AIR) || !legsBlock.getType().equals(Material.AIR)) {
+					continue;
+					
+				} else if (!floorBlock.getType().equals(Material.AIR)) {
+					
+					// potential valid spawn, check for unwanted spawn floors	
+					if (!BannedSpawnFloors.contains(floorBlock.getType())) {
+						
+						System.out.println("found valid respawn location!");
+						valid_spawn_location = true;
+						
+						newSpawnLocation.setWorld(thisWorld);
+						newSpawnLocation.setX((double)tryLocation_x);
+						newSpawnLocation.setY((double)y-1);
+						newSpawnLocation.setZ((double)tryLocation_z);
+						
+					} else continue;
+				}
+			}
+		}
+		return newSpawnLocation;
 	}
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onRespawn(PlayerRespawnEvent event) {
 		
-		System.out.println("!PlayerRespawnEvent was caught!");
-		
 		final World thisWorld = event.getRespawnLocation().getWorld();
-		Location newSpawnLocation = event.getRespawnLocation();
+		Location thisLocation = event.getRespawnLocation();
 		
 		if (Config.getValue("spawn.ignore.lava").equals("true")) {
 			BannedSpawnFloors.add(Material.LAVA);
@@ -50,72 +103,28 @@ public class SpawnController implements Listener {
 			BannedSpawnFloors.add(Material.WATER);
 		}
 		
-		// if a configured value can't be parsed to Double, set to default
-		if (config_max_x.isNaN()) max_x = 420.0; else max_x = config_max_x.doubleValue();
-		if (config_max_z.isNaN()) max_z = 420.0; else max_z = config_max_z.doubleValue();	
-		if (config_min_x.isNaN()) min_x = -420.0; else min_x = config_min_x.doubleValue();
-		if (config_min_z.isNaN()) min_z = -420.0; else min_z = config_min_z.doubleValue();
-		
 		// find then set a random spawn location if the player doesn't have a set spawn
 		if (Config.getValue("spawn.random").equals("true")) {
 			
-			System.out.println("spawn.random = true");
 			if (!event.isBedSpawn() && !event.isAnchorSpawn()) {
 				
-				System.out.println("game-intended spawn is world spawn");
-				boolean valid_spawn_location = false;
+				thisLocation = getRandomSpawn(thisWorld, thisLocation);
 				
-				// get random x, z coords and check them top-down from y256 for validity
-				while (!valid_spawn_location) {
-					
-					double tryLocation_x = getRandomNumber((int)min_x, (int)max_x);
-					double tryLocation_z = getRandomNumber((int)min_z, (int)max_z);
-					
-					System.out.println("RVAS: Checking coords for respawn: " + tryLocation_x + ", " + tryLocation_z);
-					
-					int y = 257;
-					while (y > 1) {
-						
-						Location headLoc = new Location(thisWorld, tryLocation_x, (double)y, tryLocation_z);
-						Location legsLoc = new Location(thisWorld, tryLocation_x, (double)y-1, tryLocation_z);
-						Location floorLoc = new Location(thisWorld, tryLocation_x, (double)y-2, tryLocation_z);
-						
-						Block headBlock = headLoc.getBlock();
-						Block legsBlock = legsLoc.getBlock();
-						Block floorBlock = floorLoc.getBlock();
-						
-						y--;
-						
-						if (!headBlock.getType().equals(Material.AIR) || !legsBlock.getType().equals(Material.AIR)) {
-							continue;
-							
-						} else if (!floorBlock.getType().equals(Material.AIR)) {
-							
-							// potential valid spawn, check for unwanted spawn floors	
-							if (!BannedSpawnFloors.contains(floorBlock.getType())) {
-								
-								System.out.println("found valid respawn location!");
-								valid_spawn_location = true;
-								
-								newSpawnLocation.setWorld(thisWorld);
-								newSpawnLocation.setX((double)tryLocation_x);
-								newSpawnLocation.setY((double)y-1);
-								newSpawnLocation.setZ((double)tryLocation_z);
-								
-								break;
-								
-							} else continue;
-						}
-					}
-				}
-				
-				if (newSpawnLocation != null) {
-					event.setRespawnLocation(newSpawnLocation);
+				if (thisLocation != null) {
+					event.setRespawnLocation(thisLocation);
 					return;
 				}
 			}
-			System.out.println("player has a bed or anchor spawn");
+			System.out.println(event.getPlayer().getName() + " has a bed or anchor spawn");
 		}
-		System.out.println(event.getPlayer().getName() + "'s respawn event was ignored by rvas-core.");
+		System.out.println(event.getPlayer().getName() + "'s respawn event was ignored by rvas-core");
+	}
+	
+	public void onJoin(PlayerJoinEvent event) {
+		System.out.println("PlayerJoinEvent triggered.");
+		
+		Player joiner = event.getPlayer();
+		UUID joiner_id = joiner.getUniqueId();
+		
 	}
 }
